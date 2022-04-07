131412dc637d646d3fa4f504c6ccf5ea3d5f578b
John Greer <johnmorrisgreer@gmail.com>
2022-04-06 22:29:02 -0400

    added front end but await not awaiting long enough

--- script.js
+++ script.js
@@ -19,4 +19,26 @@ const howLongToRead = async (isbn) => {
     return output
 }
 
-howLongToRead(9780141923505)
+// howLongToRead(9780141923505)
+
+const btn = document.createElement('button')
+btn.innerText = `How long to read?`
+btn.classList.add('button')
+btn.setAttribute('type','button')
+document.getElementById('form').appendChild(btn)
+
+btn.addEventListener('click', async (isbn)=>{
+    const getData = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${apiKey}`)
+    let pageCount = getData.data.items[0].volumeInfo.pageCount
+    let bookTitle = getData.data.items[0].volumeInfo.title
+    let bookAuthor = getData.data.items[0].volumeInfo.authors[0]
+    let estWordsPerPage = 250 // big ol assumption right here
+    let totalWords = (estWordsPerPage * pageCount)
+    let wordsPerMinute = 275 // 250 is the average; in the future we should custom set this
+    let timeToReadInMinutes = (totalWords / wordsPerMinute)
+    let hours = Math.floor(timeToReadInMinutes / 60)
+    let minutes = parseInt(timeToReadInMinutes % 60)
+    let output = `Reading "${bookTitle}" by ${bookAuthor} would take approximately ${hours} hours and ${minutes} minutes.`
+    console.log(output)
+    return output
+})
